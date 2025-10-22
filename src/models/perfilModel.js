a7660f478468fc73578f43bcb3751968eaa922ed
Andres_A <Andres_A@gmail.com>
2025-10-21 22:05:44 -0500

    Creacion de el delete /api/perfil/:id para eliminar un perfil mediante el id

--- src/models/perfilModel.js
+++ src/models/perfilModel.js
@@ -1,27 +1,36 @@
 const mongoose = require("mongoose");
 
-const perfilSchema = new mongoose.Schema({
-  nombre: {
-    type: String,
-    required: true,
+const perfilSchema = new mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+    },
+    correo: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true, // asegura que todos los correos estén en minúsculas
+      match: [/^\S+@\S+\.\S+$/, "Formato de correo inválido"], // validación simple de email
+    },
+    biografia: {
+      type: String,
+      default: "Sin biografía",
+      maxlength: 500, // límite de longitud opcional
+    },
+    fotoPerfil: {
+      type: String,
+      default: "default.jpg", //  imagen por defecto
+    },
+    historialPublicaciones: {
+      type: [String], // lista de títulos o IDs de artículos
+      default: [],
+    },
   },
-  correo: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  biografia: {
-    type: String,
-    default: "Sin biografía",
-  },
-  fotoPerfil: {
-    type: String,
-    default: "default.jpg", // o una URL por defecto
-  },
-  historialPublicaciones: {
-    type: [String], // lista de títulos o IDs de artículos
-    default: [],
-  },
-});
+  {
+    timestamps: true, // crea campos "createdAt" y "updatedAt" que son para llevar el control de tiempo
+    versionKey: false, // elimina el campo para la versión de los documentos (__v)
+  }
+);
 
 module.exports = mongoose.model("Perfil", perfilSchema);
--- src/routes/PerfilRoutes.js
+++ src/routes/PerfilRoutes.js
@@ -17,3 +17,13 @@ router.get("/perfil", (req, res) => {
     .then((data) => res.json(data))
     .catch((error) => res.status(500).json({ message: error.message }));
 });
+//eliminar un perfil por ID
+router.delete("/perfil/:id", (req, res) => {
+  const { id } = req.params;
+  perfilSchema
+    .deleteOne({ _id: id })
+    .then((data) => res.json({ message: "Perfil eliminado correctamente", data }))
+    .catch((error) => res.status(400).json({ message: error.message }));
+});
+
+module.exports = router;
