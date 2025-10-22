9bebedbf7ee69380ad53d9b372b513ef8b696eb6
Andres_A <Andres_A@gmail.com>
2025-10-21 21:57:13 -0500

    Creacion de la GET /api/perfil para consultar todos los perfiles

--- src/routes/PerfilRoutes.js
+++ src/routes/PerfilRoutes.js
@@ -10,3 +10,10 @@ router.post("/perfil", (req, res) => {
     .then((data) => res.json(data))
     .catch((error) => res.status(400).json({ message: error.message }));
 });
+// Consultar todos los perfiles
+router.get("/perfil", (req, res) => {
+  perfilSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.status(500).json({ message: error.message }));
+});
